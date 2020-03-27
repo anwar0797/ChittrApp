3150f858ea5d1211167d9922e6532d5ca6797e0f
anwar0797 <hashim-07@hotmail.co.uk>
2020-03-27 20:13:29 +0000

    Update Setting.js

--- ChittrAssignment/src/component/drawer/Setting.js
+++ ChittrAssignment/src/component/drawer/Setting.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Image, StyleSheet} from 'react-native';
 import {Text} from 'native-base';
 import {CustomHeader} from '../CustomHeader';
+import {IMAGE} from '../../constants/Image'
 
 export class Setting extends React.Component {
   render() {
@@ -9,9 +10,22 @@ export class Setting extends React.Component {
       <View style={{flex: 1}}>
         <CustomHeader title="Setting" navigation={this.props.navigation} />
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Text>Setting Screen </Text>
+          <Text>Setting Screen</Text>
+          <Image
+            source={IMAGE.ICON_USER_DEFAULT}
+            style={styles.photo}
+          />
         </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  photo: {
+    width: 50,
+    height: 50,
+    borderRadius: 30,
+    marginTop: 20,
+  }
+})
